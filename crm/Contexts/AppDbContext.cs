32fa62057363db379aec8b5832ac283c9abb8001
Tobias Perassi Alquezar <tobiasperassi@gmail.com>
2025-09-30 20:50:38 -0300

    Creating the AppDbContext

---
+++ crm/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+using ApiMvcSwagger.Models;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ClientModel> Clients { get; set; }
+        public DbSet<CompanyCardModel> Cards { get; set; }
+        public DbSet<CompanyModel> Company { get; set; }
+        public DbSet<HistoryModel> Historys { get; set; }
+        public DbSet<ObservationModel> Observations { get; set; }
+        public DbSet<StepColumnModel> StepColumn { get; set; }
+        public DbSet<UserModel> User { get; set; }
+        public DbSet<ErrorViewModel> ErrorViews { get; set; }
+        
+    }
+}
